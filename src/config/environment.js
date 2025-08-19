2d710c6d5e8986ca4455eccc8024645a7184ad67
Chipyoka <mwapechipyoka18@gmail.com>
2025-08-19 22:44:54 +0200

    Updated Readme

--- src/config/environment.js
+++ src/config/environment.js
@@ -13,7 +13,7 @@ const env = {
   },
   jwt: {
     secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN || '1h'
+    expiresIn: process.env.JWT_EXPIRES_IN || '2h'
   },
   hf: {
     apiKey: process.env.HF_API_KEY,
